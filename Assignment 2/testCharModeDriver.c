aa7fd0edd41010ab519fe1e74588e60367ca881a
Zachary Gill <ZGorlock@gmail.com>
2018-03-20 18:35:06 -0400

    Updated test class

--- Assignment 2/testCharModeDriver.c
+++ Assignment 2/testCharModeDriver.c
@@ -5,36 +5,160 @@ static char receive[BUFFER_LENGTH];
 
 
 int main(void) {
-   int ret, fd;
-   char stringToSend[BUFFER_LENGTH];
+	int ret, fd;
+	char stringToSend[BUFFER_LENGTH];
    
-   printf("Starting device test code example...\n");
-   fd = open("/dev/charModeDevice", O_RDWR);             // Open the device with read/write access
-   if (fd < 0){
-      perror("Failed to open the device...");
-      return errno;
-   }
-   printf("Type in a short string to send to the kernel module:\n");
-   scanf("%[^\n]%*c", stringToSend);                // Read in a string (with spaces)
-   printf("Writing message to the device [%s].\n", stringToSend);
-   ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
-   if (ret < 0){
-      perror("Failed to write the message to the device.");
-      return errno;
-   }
+	printf("Starting device test code example...\n");
+	fd = open("/dev/charModeDevice", O_RDWR);             // Open the device with read/write access
+	if (fd < 0){
+		perror("Failed to open the device...");
+		return errno;
+	}
+
+
+	printf("Testing normal read/write");
+	char stringToSend1[BUFFER_LENGTH];
+	strncpy(stringToSend1, "Testing testing 123", BUFFER_LENGTH);
+
+	ret = write(fd, stringToSend1, strlen(stringToSend1));
+	if (ret < 0){
+		perror("Failed to write the message to the device.");
+		return errno;
+	}
+	ret = read(fd, receive, strlen(stringToSend1));
+	if (ret < 0){
+		perror("Failed to read the message from the device.");
+		return errno;
+	}
+
+
+	printf("Testing max read/write");
+	char stringToSend2[BUFFER_LENGTH];
+	strncpy(stringToSend2, "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", BUFFER_LENGTH);
+
+	ret = write(fd, stringToSend2, strlen(stringToSend2));
+	if (ret < 0){
+		perror("Failed to write the message to the device.");
+		return errno;
+	}
+	ret = read(fd, receive, strlen(stringToSend2));
+	if (ret < 0){
+		perror("Failed to read the message from the device.");
+		return errno;
+	}
+
+
+	printf("Testing consecutive read/write");
+	char stringToSend3[BUFFER_LENGTH];
+	stringToSend3[0] = '1';
+
+	ret = write(fd, stringToSend3, 1);
+	if (ret < 0){
+		perror("Failed to write the message to the device.");
+		return errno;
+	}
+
+	stringToSend3[0] = '2';
+	ret = write(fd, stringToSend3, 1);
+	if (ret < 0){
+		perror("Failed to write the message to the device.");
+		return errno;
+	}
+
+	stringToSend3[0] = '3';
+	ret = write(fd, stringToSend3, 1);
+	if (ret < 0){
+		perror("Failed to write the message to the device.");
+		return errno;
+	}
+
+	ret = read(fd, receive, 1);
+	if (ret < 0){
+		perror("Failed to read the message from the device.");
+		return errno;
+	}
+
+	ret = read(fd, receive, 1);
+	if (ret < 0){
+		perror("Failed to read the message from the device.");
+		return errno;
+	}
+
+	ret = read(fd, receive, 1);
+	if (ret < 0){
+		perror("Failed to read the message from the device.");
+		return errno;
+	}
+
+
+	printf("Testing write overflow");
+	char stringToSend4[BUFFER_LENGTH];
+	strncpy(stringToSend4, "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", BUFFER_LENGTH);
+
+	ret = write(fd, stringToSend4, strlen(stringToSend4));
+	if (ret < 0){
+		perror("Failed to write the message to the device.");
+		return errno;
+	}
+
+	char stringToSend5[BUFFER_LENGTH];
+	strncpy(stringToSend5, "1111", BUFFER_LENGTH); //only 3 spaces available
+
+	ret = write(fd, stringToSend5, strlen(stringToSend5));
+	if (ret < 0){
+		perror("Failed to write the message to the device.");
+		return errno;
+	}
+
+	ret = read(fd, receive, BUFFER_LENGTH);
+	if (ret < 0){
+		perror("Failed to read the message from the device.");
+		return errno;
+	}
+
+
+	printf("Testing read underflow");
+	char stringToSend6[BUFFER_LENGTH];
+	strncpy(stringToSend6, "111", BUFFER_LENGTH);
+
+	ret = write(fd, stringToSend6, strlen(stringToSend6));
+	if (ret < 0){
+		perror("Failed to write the message to the device.");
+		return errno;
+	}
+
+	ret = read(fd, receive, 4); //only 3 characters available
+	if (ret < 0){
+		perror("Failed to read the message from the device.");
+		return errno;
+	}
+
+
+	return SUCCESS;
+
+
+
+	//printf("Type in a short string to send to the kernel module:\n");
+	//scanf("%[^\n]%*c", stringToSend);                // Read in a string (with spaces)
+	//printf("Writing message to the device [%s].\n", stringToSend);
+	//ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
+	//if (ret < 0){
+	//	perror("Failed to write the message to the device.");
+	//	return errno;
+	//}
  
-   printf("Press ENTER to read back from the device...\n");
-   getchar();
+	//printf("Press ENTER to read back from the device...\n");
+	//getchar();
  
-   printf("Reading from the device...\n");
-   ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
-   if (ret < 0){
-      perror("Failed to read the message from the device.");
-      return errno;
-   }
+	//printf("Reading from the device...\n");
+	//ret = read(fd, receive, BUFFER_LENGTH);        // Read the response from the LKM
+	//if (ret < 0){
+	//	perror("Failed to read the message from the device.");
+	//	return errno;
+	//}
    
-   printf("The received message is: [%s]\n", receive);
-   printf("End of the program\n");
+	//printf("The received message is: [%s]\n", receive);
+	//printf("End of the program\n");
    
-   return SUCCESS;
+	//return SUCCESS;
 }
